cc59f2af05834dc04baafd2bd1f2dc9b03c680ac
Kauã Machado Grathwohl <Kaua.Grathwohl@thomsonreuters.com>
2024-11-27 13:19:44 -0300

    Feat: Configurando fluxo conforme erros do workflow

--- src/tests/thecatapi/GetRandomImage.spec.js
+++ src/tests/thecatapi/GetRandomImage.spec.js
@@ -1,4 +1,5 @@
 import { htmlReport } from 'https://raw.githubusercontent.com/benc-uk/k6-reporter/main/dist/bundle.js';
+import { textSummary } from 'https://jslib.k6.io/k6-summary/0.0.1/index.js';
 import http from 'k6/http';
 import { check, sleep } from 'k6';
 import { Trend } from 'k6/metrics';
